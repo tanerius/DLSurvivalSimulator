26b29ecb78e037ad7abd4c89df671ce16c990149
Taner Selim <tanerius@gmail.com>
2023-04-28 20:25:21 +0200

    .

--- src/Interfaces/Helpers.h
+++ src/Interfaces/Helpers.h
@@ -18,17 +18,6 @@ namespace DLS
         bool IsValid() { return (X > -1 && Y > -1); }
     };
 
-    struct Polar
-    {
-        float r;
-        float theta; // in radian
-
-        Vector2D ToCartesian() const
-        {
-            return {r * std::cos(theta), r * std::sin(theta)};
-        }
-    };
-
     struct Vector2D
     {
         float x; // r in polar
@@ -51,6 +40,17 @@ namespace DLS
         static float Distance(Vector2D v1, Vector2D v2)
         {
             return sqrt((v2.x-v1.x)*(v2.x-v1.x) + (v2.y-v1.y)*(v2.y-v1.y));
+        }
+    };
+
+    struct Polar
+    {
+        float r;
+        float theta; // in radian
+
+        Vector2D ToCartesian() const
+        {
+            return {r * std::cos(theta), r * std::sin(theta)};
         }
     };
 }
